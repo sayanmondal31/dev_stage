2d853de917f574353f200b040427e8572e00d140
debajyatibanerjee0002 <debajyatibanerjee0002@gmail.com>
2020-10-27 01:07:45 +0530

    Add file manager ui

---
+++ Mobile Application Development/Flutter/Flutter_ui/custom_ui/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:custom_ui/screens/home_page.dart';
+import 'package:custom_ui/screens/next_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      // home: NextScreen(),
+    );
+  }
+}
